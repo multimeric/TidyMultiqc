df2478af5296d2d4cd8f67f96e8917afdfd695e6
Michael Milton <michael.r.milton@gmail.com>
2021-11-26 21:45:32 +1100

    Add tests and examples for custom parsers

---
+++ tests/testthat/test-parsers.R
@@ -0,0 +1,20 @@
+test_that("We can provide custom plot parsers", {
+  report = load_multiqc(
+    system.file("extdata", "wgs/multiqc_data.json", package = "TidyMultiqc"), 
+    sections = 'plot',
+    plots = "fastqc_per_sequence_quality_scores_plot",
+    plot_parsers = list(
+      # This fake parser function takes a plot and just returns the iris dataset
+      xy_line = function(plot_data, name){
+        list(
+          sample_1 = list(
+            plot_name = list(iris)
+          )
+        )
+      }
+    )
+  )
+  
+  report$plot_name %>% 
+    purrr::map(~expect_equal(., iris))
+})
